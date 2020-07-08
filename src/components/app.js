2a619554d2de0afb6e85c802c658af38bb50c662
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-07-08 15:11:22 -0500

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+export default class App extends Component {
+    render() {
+        
+        return ( 
+            
+            <div className = 'app'>
+                <h1>Terry Roberts Portfolio</h1>
+                <h3>Just testing the application.</h3>
+            </div>
+        );
+    }
+}
