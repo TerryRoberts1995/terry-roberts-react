2dd75a50eabcb0afe2bc1a74c3868debafd1e7a5
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-07-17 14:56:27 -0500

    added dynamic error

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,16 +1,78 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: "",
+            password: "",
+            errorText: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+            errorText: ""
+        })
+    }
+
+    handleSubmit(event) {
+        axios.post("https://api.devcamp.space/sessions",
+            {
+                client: {
+                    email: this.state.email,
+                    password: this.state.password
+                }
+            },
+            { withCredentials: true }
+        ).then(response => {
+            if (response.data.status === "created") {
+                console.log("you can come in...")
+            } else {
+                this.setState({
+                    errorText: "wrong email or password"
+                })
+            }
+        }).catch(error => {
+            console.log("some error occured", error);
+            this.setState({
+                errorText: "An updated has occured."
+            })
+        });
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div >
                 <h1>Login to access dashboard.</h1>
+                <h3>{this.state.errorText}</h3>
 
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
 
-                <form>
-                    <input type="text" />
-                    <input type="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Your password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
             </div>
         );
