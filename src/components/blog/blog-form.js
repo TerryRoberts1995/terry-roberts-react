f429b61180295540b6f04de98fe56d7df9eef33b
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-07-30 16:41:39 -0500

    added editor to the modal

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import RichTextEditor from '../forms/rich-text-editor'
 
 export default class BlogForm extends Component {
     constructor(props) {
@@ -59,6 +60,10 @@ export default class BlogForm extends Component {
                         name="blog_status"
                         placeholder="Blog Status"
                         value={this.state.blog_status} />
+                </div>
+
+                <div className="one-column">
+                    <RichTextEditor />
                 </div>
 
                 <button className="btn">Save</button>
