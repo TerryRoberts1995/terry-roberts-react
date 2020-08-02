598899aeda1a66ddda896f7d499e6a043c29c90e
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-08-02 02:00:52 -0500

    added edit page component

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
@@ -13,6 +13,8 @@ export default class RichTextEditor extends Component {
         }
 
         this.onEditorStateChange = this.onEditorStateChange.bind(this);
+        this.getBase64 = this.getBase64.bind(this);
+        this.uploadFile = this.uploadFile.bind(this);
     }
 
     onEditorStateChange(editorState) {
@@ -22,8 +24,20 @@ export default class RichTextEditor extends Component {
         )
     }
 
+    getBase64(file, callback) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = () => callback(reader.result);
+        reader.onerror = error => {
+
+        };
+    }
+
     uploadFile(file) {
-        console.log("file to upload ", file);
+        return new Promise((resolve, reject) => {
+            this.getBase64(file, data => resolve({ data: { link: data } }));
+        })
     }
 
     render() {
